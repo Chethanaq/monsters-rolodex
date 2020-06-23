9c021aea622981025a84dbd24046c35dc9b37360
Chethanaq <chethanaamin330@gmail.com>
2020-06-23 18:46:33 +0530

    Updates

---
+++ precache-manifest.652a27efd831e749d74adf3cef9b6a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b7e4a77a2ac8bcb3be61190522414f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "db3a0cee5d2046f8c842",
+    "url": "/monsters-rolodex/static/css/main.f8808951.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3a0cee5d2046f8c842",
+    "url": "/monsters-rolodex/static/js/main.4ac04759.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
